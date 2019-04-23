ffab4d7fb9ce336b0cc61d70a416558181b41d36
IbraimovAzamat <43143481+IbraimovAzamat@users.noreply.github.com>
2019-04-23 23:42:30 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,23 +18,19 @@ public class Main {
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        
+
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
         JButton start = new JButton("Start");
         buttonBox.add(start);
-        
-        JButton stop = new JButton("Stop");
-        stop.addActionListener(new MyStopListener());
-        buttonBox.add(stop);
-        
-        JButton upTempo = new JButton("Tempo Up");
-        upTempo.addActionListener(new MyUpTempoListener());
-        buttonBox.add(upTempo);
-        
-        JButton downTempo = new JButton("Tempo Down");
-        downTempo.addActionListener(new MyDownTempoListener());
-        buttonBox.add(downTempo);
 
+        JButton stop = new JButton("Stop");
+        buttonBox.add(stop);
+
+        JButton upTempo = new JButton("Tempo Up");
+        buttonBox.add(upTempo);
+
+        JButton downTempo = new JButton("Tempo Down");
+        buttonBox.add(downTempo);
 
     }
 }
