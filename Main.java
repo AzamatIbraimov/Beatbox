2c9af569993e3a1e0c3456cf8ec99025bc65e7c2
IbraimovAzamat <43143481+IbraimovAzamat@users.noreply.github.com>
2019-04-23 23:31:07 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
 package YNWA;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 
 public class Main {
+    JFrame theFrame;
+
+
     public static void main(String[] args) {
         new Main().startUp();
     }
     public void startUp() {
         buildGUI();
+    }
+    public void buildGUI() {
+        theFrame = new JFrame("BeatBox");
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
     }
 }
