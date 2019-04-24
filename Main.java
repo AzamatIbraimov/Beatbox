2984fda968616131b4efb5c200fa6b1dd3a3de1b
IbraimovAzamat <43143481+IbraimovAzamat@users.noreply.github.com>
2019-04-24 22:18:33 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -127,4 +127,14 @@ public class Main {
             }
         }
     }
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+        }
+        return event;
+    }
 }
