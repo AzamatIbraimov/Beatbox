9a6b6da7b4ed5cf03c0a3ed3ce9ac009cbf60c62
IbraimovAzamat <43143481+IbraimovAzamat@users.noreply.github.com>
2019-04-24 21:08:07 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,27 @@
 package YNWA;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class Main {
     JFrame theFrame;
-   
+    JPanel mainPanel;
+    ArrayList<JCheckBox> checkboxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+
     String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat",
             "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo",
             "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibrslap", "Low-mod Tom",
             "High Agogo", "Open Hi Conga"};
-    
-    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
+   // int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
 
     public static void main(String[] args) {
@@ -39,8 +48,11 @@ public class Main {
 
         JButton downTempo = new JButton("Tempo Down");
         buttonBox.add(downTempo);
-        
-       
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i < 16; i++) {
+            nameBox.add(new Label(instrumentNames[i]));
+        }
+
         background.add(BorderLayout.EAST, buttonBox);
         background.add(BorderLayout.WEST, nameBox);
 
@@ -48,12 +60,14 @@ public class Main {
         GridLayout grid = new GridLayout(16, 16);
         grid.setVgap(1);
         grid.setHgap(2);
-    
+        mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
 
         for (int i = 0; i < 256; i++) {
             JCheckBox c = new JCheckBox();
             c.setSelected(false);
-
+           checkboxList.add(c);
+            mainPanel.add(c);
         }
 
         theFrame.setBounds(50, 50, 300, 300);
@@ -70,7 +84,5 @@ public class Main {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
-
     }
 }
