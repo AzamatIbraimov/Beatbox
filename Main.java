534cfe88f03fe33e447d6da3a860e3a06caebf22
IbraimovAzamat <43143481+IbraimovAzamat@users.noreply.github.com>
2019-04-25 23:03:19 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 package YNWA;
+
 import javax.sound.midi.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
+import java.util.Iterator;
 public class Main {
     JFrame theFrame;
     JPanel mainPanel;
@@ -11,42 +14,57 @@ public class Main {
     Sequencer sequencer;
     Sequence sequence;
     Track track;
+    float tempoFactor = 1;
 
     String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat",
             "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo",
             "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibrslap", "Low-mod Tom",
             "High Agogo", "Open Hi Conga"};
 
-   // int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
-
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
     public static void main(String[] args) {
         new Main().startUp();
     }
+
     public void startUp() {
+        setUpMidi();
         buildGUI();
+
     }
+
     public void buildGUI() {
         theFrame = new JFrame("BeatBox");
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        checkboxList = new ArrayList<JCheckBox>();
+
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
         JButton start = new JButton("Start");
+        start.setSize(100,100);
+        start.addActionListener(new MyStartListener());
         buttonBox.add(start);
 
         JButton stop = new JButton("Stop");
+        stop.setSize(100,100);
+        stop.addActionListener(new MyStopListener());
         buttonBox.add(stop);
 
         JButton upTempo = new JButton("Tempo Up");
+        upTempo.setSize(100,100);
+        upTempo.addActionListener(new MyUpTempoListener());
         buttonBox.add(upTempo);
-        
-        JButton Speed = new JButton("Speed");
-        buttonBox.add(Speed);
 
         JButton downTempo = new JButton("Tempo Down");
+        downTempo.setSize(100,100);
+        downTempo.addActionListener(new MyDownTempoListener());
         buttonBox.add(downTempo);
+
+        JLabel speed=new JLabel(String.valueOf(tempoFactor));
+        buttonBox.add(speed);
+
         Box nameBox = new Box(BoxLayout.Y_AXIS);
         for (int i = 0; i < 16; i++) {
             nameBox.add(new Label(instrumentNames[i]));
@@ -65,7 +83,7 @@ public class Main {
         for (int i = 0; i < 256; i++) {
             JCheckBox c = new JCheckBox();
             c.setSelected(false);
-           checkboxList.add(c);
+            checkboxList.add(c);
             mainPanel.add(c);
         }
 
@@ -73,6 +91,7 @@ public class Main {
         theFrame.pack();
         theFrame.setVisible(true);
     }
+
     public void setUpMidi() {
         try {
             sequencer = MidiSystem.getSequencer();
@@ -84,39 +103,64 @@ public class Main {
             ex.printStackTrace();
         }
     }
-    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
-        MidiEvent event = null;
-        try {
-            ShortMessage a = new ShortMessage();
-            a.setMessage(comd, chan, one, two);
-            event = new MidiEvent(a, tick);
-        } catch (Exception e) {
+
+    public void buildTrackAndStart() {
+        ArrayList<Integer> trackList = null;
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new ArrayList<>();
+
+            for (int j = 0; j < 16; j++) {
+                JCheckBox jc = (JCheckBox) checkboxList.get(j + (16 * i));
+                if (jc.isSelected()) {
+                    int key = instruments[i];
+                    trackList.add(new Integer(key));
+                } else {
+                    trackList.add(null);
+                }
+            }
+            makeTracks(trackList);
         }
-        return event;
+        track.add(makeEvent(192, 9, 1, 0, 15));
+        try {
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     public class MyStartListener implements ActionListener {
         public void actionPerformed(ActionEvent a) {
             buildTrackAndStart();
         }
     }
+
     public class MyStopListener implements ActionListener {
         public void actionPerformed(ActionEvent a) {
             sequencer.stop();
         }
     }
+
     public class MyUpTempoListener implements ActionListener {
         public void actionPerformed(ActionEvent a) {
-            float tempoFactor = sequencer.getTempoFactor();
+            tempoFactor = sequencer.getTempoFactor();
             sequencer.setTempoFactor((float) (tempoFactor * 1.03));
         }
     }
-     public class MyDownTempoListener implements ActionListener {
+
+    public class MyDownTempoListener implements ActionListener {
         public void actionPerformed(ActionEvent a) {
-            float tempoFactor = sequencer.getTempoFactor();
+            tempoFactor = sequencer.getTempoFactor();
             sequencer.setTempoFactor((float) (tempoFactor * .97));
         }
     }
-     public void makeTracks(ArrayList list) {
+
+    public void makeTracks(ArrayList list) {
         Iterator it = list.iterator();
         for (int i = 0; i < 16; i++) {
             Integer num = (Integer) it.next();
@@ -127,6 +171,7 @@ public class Main {
             }
         }
     }
+
     public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
         MidiEvent event = null;
         try {
@@ -137,4 +182,6 @@ public class Main {
         }
         return event;
     }
+
+
 }
